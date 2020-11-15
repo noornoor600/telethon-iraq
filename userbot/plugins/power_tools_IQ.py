25a16bc62a983bf9ae87deda36dad0fa52a8a6e0
i.q <64720410+klanrali@users.noreply.github.com>
2020-11-15 18:48:02 +0300

    Update and rename power_tools.py to power_tools_IQ.py

--- userbot/plugins/power_tools_IQ.py
+++ userbot/plugins/power_tools_IQ.py
@@ -2,9 +2,6 @@
 Available Commands:
 .restart
 .shutdown"""
-# This Source Code Form is subject to the terms of the GNU
-# General Public License, v.3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
 from telethon import events
 import asyncio
 import os
@@ -17,11 +14,11 @@ async def _(event):
     if event.fwd_from:
         return
     # await asyncio.sleep(2)
-    # await event.edit("Restarting [██░] ...\n`.ping` me or `.help` to check if I am online after a lil bit.")
+    # await event.edit("Restarting [██░] ...\n`.ping` me or `.alive` to check if I am online after a lil bit.")
     # await asyncio.sleep(2)
-    # await event.edit("Restarting [███]...\n`.ping` me or `.help` to check if I am online after a lil bit.")
+    # await event.edit("Restarting [███]...\n`.ping` me or `.alive` to check if I am online after a lil bit.")
     # await asyncio.sleep(2)
-    await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
+    await event.edit("Restarted. `.ping` me or `.alive` to check if I am online")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
