b6dafb7ed1a3689f49752bd1b64399c93e559e2a
i.q <64720410+klanrali@users.noreply.github.com>
2020-11-15 23:43:16 +0300

    Update and rename eval.py to eval_IQ.py

--- userbot/plugins/eval_IQ.py
+++ userbot/plugins/eval_IQ.py
@@ -1,8 +1,6 @@
+#KLANR @IQTHON
 """Evaluate Python Code inside Telegram
 Syntax: .eval PythonCode"""
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from telethon import events, errors, functions, types
 import inspect
