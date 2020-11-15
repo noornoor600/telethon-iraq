6baadbef8cf93d12e08ec820f559932034d3ad7c
i.q <64720410+klanrali@users.noreply.github.com>
2020-11-16 00:27:20 +0300

    Update and rename lydia.py to lydia_IQ.py

--- userbot/plugins/lydia_IQ.py
+++ userbot/plugins/lydia_IQ.py
@@ -1,7 +1,3 @@
-#   Copyright 2019 - 2020 DarkPrinc3
-
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
 #   KLANR @IQTHON
 
 from coffeehouse.lydia import LydiaAI
