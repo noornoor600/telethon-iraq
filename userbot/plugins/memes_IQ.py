8e7e36121b8610b39b9b5994f2c149e5f34e3219
i.q <64720410+klanrali@users.noreply.github.com>
2020-11-15 23:55:29 +0300

    Update and rename memes.py to memes_IQ.py

--- userbot/plugins/memes_IQ.py
+++ userbot/plugins/memes_IQ.py
@@ -1,9 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.b (the "License");
-# you may not use this file except in compliance with the License.
-#
-#
+#KLANR @IQTHON
 
 """ Userbot module for having some fun with people. """
 
