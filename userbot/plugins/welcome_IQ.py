1661d6847c1173ae5d74eb9e53dbbd783ad3947c
i.q <64720410+klanrali@users.noreply.github.com>
2020-11-16 16:36:15 +0300

    Update welcome_IQ.py

--- userbot/plugins/welcome_IQ.py
+++ userbot/plugins/welcome_IQ.py
@@ -60,7 +60,7 @@ async def _(event):
     else:
         input_str = event.text.split(None, 1)
         add_welcome_setting(event.chat_id, input_str[1], True, 0, None)
-        await event.edit("Welcome note saved. ")
+        await event.edit("-  𖢞 Welcome saved - @IQTHON. ")
 
 
 @command(pattern="^.clearwelcome")  # pylint:disable=E0602
@@ -70,7 +70,7 @@ async def _(event):
     cws = get_current_welcome_settings(event.chat_id)
     rm_welcome_setting(event.chat_id)
     await event.edit(
-        "Welcome note cleared. " + \
+        "-  𖢞 Welcome cleared - @IQTHON. " + \
         "The previous welcome message was `{}`.".format(cws.custom_welcome_message)
     )
 
