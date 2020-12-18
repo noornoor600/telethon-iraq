f958af26e8686f432760ae9b0fce90b94d1d731a
i.q <64720410+klanrali@users.noreply.github.com>
2020-12-18 17:10:02 +0300

    Update bye_IQ.py

--- userbot/plugins/bye_IQ.py
+++ userbot/plugins/bye_IQ.py
@@ -11,7 +11,7 @@ import time
 @borg.on(admin_cmd("bye", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I am leaving this chat.....!`")
+        await e.edit("I am leaving this chat.. - @IQTHON")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
